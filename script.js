5bb7d23ee6538b7bb261f0082db8207519ee6454
Fware0037 <115996223+Fware0037@users.noreply.github.com>
2024-05-29 16:33:43 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+// Check if promo code is used
+let promoUsed = localStorage.getItem("promoUsed") === "true";
+
+// Check if bitcoin count is available in local storage
+let bitcoinCount = parseFloat(localStorage.getItem("bitcoinCount")) || 0;
+
+// Update bitcoin counter display
+updateBitcoinCounter();
+
+// Bitcoin button click handler
+document.getElementById("bitcoinButton").addEventListener("click", function() {
+    bitcoinCount += 0.0005000;
+    updateBitcoinCounter();
+});
+
+// Function to update bitcoin counter display
+function updateBitcoinCounter() {
+    document.getElementById("bitcoinCounter").innerText = bitcoinCount.toFixed(3) + " BTC";
+    // Save bitcoin count to local storage
+    localStorage.setItem("bitcoinCount", bitcoinCount);
+}
+
+// Apply promo code button click handler
+document.getElementById("applyPromoButton").addEventListener("click", function() {
+    applyPromoCode();
+});
+
+// Function to apply promo code
+function applyPromoCode() {
+    const promoInputValue = document.getElementById("promoInput").value.toUpperCase();
+    const promoCodes = JSON.parse(localStorage.getItem("promoCodes")) || [];
+    if (!promoUsed && promoCodes.includes(promoInputValue)) {
+        switch (promoInputValue) {
+            case "FREEBTC":
+                bitcoinCount += 0.5;
+                break;
+            case "bin":
+                bitcoinCount -= Math.ceil(bitcoinCount * 6.75);
+                break;
+            case "H8678-12HJDF-7777":
+                bitcoinCount += 1488;
+                break;
+            default:
+                break;
+        }
+        updateBitcoinCounter();
+        promoUsed = true;
+        localStorage.setItem("promoUsed", "true");
+        alert("Promo code applied successfully!");
+    } else {
+        alert("Invalid or already used promo code!");
+    }
+}
